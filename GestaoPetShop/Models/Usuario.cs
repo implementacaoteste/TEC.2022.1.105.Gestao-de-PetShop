1692a7efba080d2a60c63c366cb565843b8df06c
Daniel Germano Silva <ddger2018@gmail.com>
2023-07-05 20:18:56 -0300

    CORRECAO USUARIOS MODELS

--- GestaoPetShop/Models/Usuario.cs
+++ GestaoPetShop/Models/Usuario.cs
@@ -5,12 +5,11 @@ namespace Models
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string NomeUsuario { get; set; }
+        public string Login { get; set; }
         public string Senha { get; set; }
-        public string Email { get; set; }
-        public string CPF { get; set; }
-        public bool Ativo { get; set; }
+        public int IdProfissional { get; set; }
+
+
         public List<GrupoUsuario> GrupoUsuarios { get; set; }
     }
 }
