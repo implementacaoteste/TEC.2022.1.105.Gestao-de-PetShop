65d423f46b621c2976641e6e6c9efb826b5fc284
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-07-11 13:58:12 -0300

    11-07-2023 dimas v1

--- GestaoPetShop/Models/Servico.cs
+++ GestaoPetShop/Models/Servico.cs
@@ -9,7 +9,10 @@ namespace Models
     public class Servico
     {
         public int Id { get; set; }
-        public string    ()
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Tempo { get; set; }
+        public bool Ativo { get; set; }
 
     }
 }
