343338aac75e8ae7f028b12f2885cd3635f38520
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-07-11 09:51:03 -0300

    11-7-23

---
+++ GestaoPetShop/Models/Servico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+        public string    ()
+
+    }
+}
