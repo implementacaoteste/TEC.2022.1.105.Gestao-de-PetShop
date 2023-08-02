5869b11eccd727f39f567d4caf9463f9e29ffcd9
Railson Sousa <123516631+railsonsousa10@users.noreply.github.com>
2023-08-01 21:40:13 -0300

    01/08 Criação Models
    Hoje foram criada as Classes Models animal e raça, também foi iniciado a criação da Classe Animal DAL e o método inserir

---
+++ GestaoPetShop/Models/Raca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Raca
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string Especie { get; set; }
+        public string PaisOrigem { get; set; }
+
+    }
+}
