23d73be5932bda7fd8dbc24c757d1b4e6bb9d5ef
Railson Sousa <123516631+railsonsousa10@users.noreply.github.com>
2023-08-04 21:38:37 -0300

    Método buscar por nome concluído

--- GestaoPetShop/DAL/RacaDAL.cs
+++ GestaoPetShop/DAL/RacaDAL.cs
@@ -106,7 +106,28 @@ namespace DAL
             {
                 cn.Close();
             }
-        
         }
+        public void Excluir(int _id)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"DELETE FROM Raca WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", _id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar excluir raça no banco de dados.", ex) { Data = { { "Id", 201 } } };
+            }
+        }
+
     }
 }
