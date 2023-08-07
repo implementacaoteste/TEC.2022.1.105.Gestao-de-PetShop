309c6396e543ba9c4f1402dd62bbe9118b22de3e
DanielGermanosilva <ddger2018@gmail.com>
2023-08-07 20:27:08 -0300

    Update ProfissionalDAL.cs

--- GestaoPetShop/DAL/ProfissionalDAL.cs
+++ GestaoPetShop/DAL/ProfissionalDAL.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DAL
+﻿namespace DAL
 {
     internal class ProfissionalDAL
     {
+        public void Inserir(Profissional _profissional) 
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Profissonal(idfuncao, Nome, CPF, Logradouro, Numero, Bairro, Cidade, UF, Pais, CEP, DataNascimento,Foto,Ativo) 
+                                    VALUES(@idfuncao, @Nome, @CPF, @Logradouro, @Numero, @Bairro, @Cidade, @UF, @Pais, @CEP, @DataNascimento,@Foto,@Ativo)";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.parameters.addwithvalue("@idfuncao", _profissional.idfuncao);
+                cmd.Parameters.AddWithValue("@Nome", _profissional.Nome);
+                cmd.Parameters.AddWithValue("@CPF", _profissional.CPF);
+                cmd.Parameters.AddWithValue("@Logradouro", _profissional.Logradouro);
+                cmd.Parameters.AddWithValue("@Numero", _profissional.Numero);
+                cmd.Parameters.AddWithValue("@Bairro", _profissional.Bairro);
+                cmd.Parameters.AddWithValue("@Cidade", _profissional.Cidade);
+                cmd.Parameters.AddWithValue("@UF", _profissional.UF);
+                cmd.Parameters.AddWithValue("@Pais", _profissional.Pais);
+                cmd.Parameters.AddWithValue("@CEP", _profissional.CEP);
+                cmd.Parameters.AddWithValue("@DataNascimento", _profissional.DataNascimento);
+                cmd.Parameters.AddWithValue("@Foto", _profissional.Foto);
+                cmd.Parameters.AddWithValue("@Ativo", _profissional.Ativo);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um Profissional no banco de dados.", ex) { Data = { { "Id", 50 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
     }
 }
