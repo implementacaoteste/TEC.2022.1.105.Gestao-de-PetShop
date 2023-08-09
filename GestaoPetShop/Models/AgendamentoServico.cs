facb47191eb22931c8a24c6dc3e8d5a169e4e7ab
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-08-09 19:44:22 -0300

    09-08-2023 correção de erro na model Agendamento serviço

---
+++ GestaoPetShop/Models/AgendamentoServico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AgendamentoServico
+    {
+       
+            public int Id { get; set; }
+            public string Servico { get; set; }
+            public int Quantidade { get; set; }
+            public decimal ValorUnitario { get; set; }
+            public decimal ValorComDesconto { get; set; }
+            public decimal ValorTotal { get; set; }
+            public int IdServico { get; set; }
+       
+    }
+}
