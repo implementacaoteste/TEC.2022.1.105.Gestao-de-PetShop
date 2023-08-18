8d2ce805fe4ccc15e0153f7b1a2c010f879096ef
DanielGermanosilva <ddger2018@gmail.com>
2023-08-17 21:17:06 -0300

    Create TelefoneProfissional.cs

---
+++ GestaoPetShop/Models/TelefoneProfissional.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class TelefoneProfissional
+    {
+        public int Id { get; set; }
+        public int IdProfissional { get; set; }
+        public string Telefone { get; set; }
+    }
+}
