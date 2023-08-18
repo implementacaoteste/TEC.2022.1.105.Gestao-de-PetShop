beb427e46826cc6a844738eec23950745b2f768d
DanielGermanosilva <ddger2018@gmail.com>
2023-08-17 21:47:16 -0300

    Update ProfissionalDAL.cs

--- GestaoPetShop/DAL/ProfissionalDAL.cs
+++ GestaoPetShop/DAL/ProfissionalDAL.cs
@@ -58,61 +58,106 @@ namespace DAL
                 cn.Close();
             }
         }
+        /*
+                private void InserirTelefoneProfissional(Profissional _profissional, int _idprofissional, SqlTransaction _transaction)
+                {
+                    SqlTransaction transaction = _transaction;
+                    List<Profissional> profissional = new List<Profissional>();
+
+                    using (SqlConnection cn = new SqlConnection(Conexao.StringDeConexao))
+                    {
+                        using (SqlCommand cmd = new SqlCommand(@"INSERT INTO TelefoneProfissional (IdProfissional,Telefone)
+                                                                                      VALUES (@IdProfissional,@Telefone)", cn))
+                        {
+                            try
+                            {
+                                if (transaction == null)
+                                {
+                                    cn.Open();
+                                    transaction = cn.BeginTransaction();
+                                }
+
+                                cmd.Transaction = transaction;
+                                cmd.Connection = transaction.Connection;
+
+                                foreach (Profissional item in _profissional.TelefoneProfissional)
+
+                                    cmd.Parameters.Clear();
+                                cmd.Parameters.AddWithValue("@IdProfissional", _idprofissional);
+                                cmd.Parameters.AddWithValue("@Telefone", item.TelefoneProfissional);
 
 
-        public List<Profissional> BuscarPorNome(string _nome)
-        {
-            throw new NotImplementedException();
-        }
+                                cmd.ExecuteNonQuery();
 
-        public List<Profissional> BuscarTodos()
-        {
-            throw new NotImplementedException();
-        }
 
-        public void Excluir(int _id)
-        {
-            throw new NotImplementedException();
-        }
+                                if (_transaction == null)
+                                    transaction.Commit();
+                            }
+                            catch (Exception ex)
+                            {
+                                transaction.Rollback();
+                                throw new Exception("Ocorreu um erro ao tentar inserir um telefone no banco de dados.", ex) { Data = { { "Id", 57 } } };
+                            }
+                        }
+                    }
+                }
 
-        public void Inserir(Profissional _profissional)
-        {
-            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
-            try
+                    public List<Profissional> BuscarPorNome(string _nome)
+                    {
+                        throw new NotImplementedException();
+                    }
+
+                    public List<Profissional> BuscarTodos()
+                    {
+                        throw new NotImplementedException();
+                    }
+
+                    public void Excluir(int _id)
+                    {
+                        throw new NotImplementedException();
+                    }
+        */
+ 
+            public void Inserir(Profissional _profissional)
             {
-                SqlCommand cmd = cn.CreateCommand();
-                cmd.CommandText = @"INSERT INTO Profissonal(idfuncao, Nome, CPF, Logradouro, Numero, Bairro, Cidade, UF, Pais, CEP, DataNascimento,Foto,Ativo) 
+                SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+                try
+                {
+                    SqlCommand cmd = cn.CreateCommand();
+                    cmd.CommandText = @"INSERT INTO Profissonal(idfuncao, Nome, CPF, Logradouro, Numero, Bairro, Cidade, UF, Pais, CEP, DataNascimento,Foto,Ativo) 
                                     VALUES(@idfuncao, @Nome, @CPF, @Logradouro, @Numero, @Bairro, @Cidade, @UF, @Pais, @CEP, @DataNascimento,@Foto,@Ativo)";
 
-                cmd.CommandType = System.Data.CommandType.Text;
-                cmd.Parameters.AddWithValue("@idfuncao", _profissional.IdFuncao);
-                cmd.Parameters.AddWithValue("@Nome", _profissional.Nome);
-                cmd.Parameters.AddWithValue("@CPF", _profissional.CPF);
-                cmd.Parameters.AddWithValue("@Logradouro", _profissional.Logradouro);
-                cmd.Parameters.AddWithValue("@Numero", _profissional.Numero);
-                cmd.Parameters.AddWithValue("@Bairro", _profissional.Bairro);
-                cmd.Parameters.AddWithValue("@Cidade", _profissional.Cidade);
-                cmd.Parameters.AddWithValue("@UF", _profissional.UF);
-                cmd.Parameters.AddWithValue("@Pais", _profissional.Pais);
-                cmd.Parameters.AddWithValue("@CEP", _profissional.CEP);
-                cmd.Parameters.AddWithValue("@DataNascimento", _profissional.DataNascimento);
-                cmd.Parameters.AddWithValue("@Foto", _profissional.Foto);
-                cmd.Parameters.AddWithValue("@Ativo", _profissional.Ativo);
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Parameters.AddWithValue("@idfuncao", _profissional.IdFuncao);
+                    cmd.Parameters.AddWithValue("@Nome", _profissional.Nome);
+                    cmd.Parameters.AddWithValue("@CPF", _profissional.CPF);
+                    cmd.Parameters.AddWithValue("@Logradouro", _profissional.Logradouro);
+                    cmd.Parameters.AddWithValue("@Numero", _profissional.Numero);
+                    cmd.Parameters.AddWithValue("@Bairro", _profissional.Bairro);
+                    cmd.Parameters.AddWithValue("@Cidade", _profissional.Cidade);
+                    cmd.Parameters.AddWithValue("@UF", _profissional.UF);
+                    cmd.Parameters.AddWithValue("@Pais", _profissional.Pais);
+                    cmd.Parameters.AddWithValue("@CEP", _profissional.CEP);
+                    cmd.Parameters.AddWithValue("@DataNascimento", _profissional.DataNascimento);
+                    cmd.Parameters.AddWithValue("@Foto", _profissional.Foto);
+                    cmd.Parameters.AddWithValue("@Ativo", _profissional.Ativo);
 
-                cmd.Connection = cn;
-                cn.Open();
+                    cmd.Connection = cn;
+                    cn.Open();
 
-                cmd.ExecuteNonQuery();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Ocorreu um erro ao tentar inserir um Profissional no banco de dados.", ex) { Data = { { "Id", 50 } } };
+                }
+                finally
+                {
+                    cn.Close();
+                }
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Ocorreu um erro ao tentar inserir um Profissional no banco de dados.", ex) { Data = { { "Id", 50 } } };
-            }
-            finally
-            {
-                cn.Close();
-            }
-        }
+        
     }
 }
+
 
