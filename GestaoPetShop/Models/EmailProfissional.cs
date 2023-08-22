6f2c87f57e512b8e0f333921377ed981bea368a0
DanielGermanosilva <ddger2018@gmail.com>
2023-08-22 20:03:02 -0300

    Update EmailProfissional.cs

--- GestaoPetShop/Models/EmailProfissional.cs
+++ GestaoPetShop/Models/EmailProfissional.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class EmailProfissional
+    public class EmailProfissional
     {
         public int Id { get; set; }
         public int IdProfissional { get; set; }
