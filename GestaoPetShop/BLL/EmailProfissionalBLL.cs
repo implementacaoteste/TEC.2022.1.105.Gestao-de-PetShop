535cc34fafc17553b6bb624e104c08db96c5f6bf
MilenaCalacio <milenanascimento71140@gmail.com>
2023-09-01 21:45:01 -0300

    Atualizações
    Telefone Profissional
    Email Profissional

---
+++ GestaoPetShop/BLL/EmailProfissionalBLL.cs
@@ -0,0 +1,23 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class EmailProfissionalBLL
+    {
+        public void Inserir(EmailProfissional _emailProfissional)
+        {
+            new EmailProfissionalDAL().Inserir(_emailProfissional);
+        }
+       /* public void Alterar(EmailProfissional _emailProfissional)
+        {
+            new EmailProfissionalDAL().Alterar(_emailProfissional);
+        }*/
+    }
+
+}
