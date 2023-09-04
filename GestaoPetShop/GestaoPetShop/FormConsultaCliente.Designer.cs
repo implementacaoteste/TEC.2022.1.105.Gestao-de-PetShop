000aea06ef37ec6db37fc8e81f0595364578aec5
MilenaCalacio <milenanascimento71140@gmail.com>
2023-09-04 20:48:51 -0300

    Ajustes

--- GestaoPetShop/GestaoPetShop/FormConsultaCliente.Designer.cs
+++ GestaoPetShop/GestaoPetShop/FormConsultaCliente.Designer.cs
@@ -381,6 +381,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(889, 438);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.clienteDataGridView);
