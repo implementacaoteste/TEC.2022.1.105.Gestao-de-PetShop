bcec5081d324327762c9a8b4419aa45aca40c25d
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-09-13 21:10:57 -0300

    Criando uma nova classe senha

--- GestaoPetShop/GestaoPetShop/FormCadastroProfissional.Designer.cs
+++ GestaoPetShop/GestaoPetShop/FormCadastroProfissional.Designer.cs
@@ -527,6 +527,7 @@
             this.button1.TabIndex = 41;
             this.button1.Text = "Cadastrar Usuário";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormCadastroProfissional
             // 
---
+++ GestaoPetShop/GestaoPetShop/FormGerarSenha.cs
@@ -0,0 +1,78 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestaoPetShop
+{
+    public partial class FormGerarSenha : Form
+    {
+        string senhaAtual = "teste123";
+        public FormGerarSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Senha.GerarSenha(8));
+
+            if (txtSenhaAtual.Text == senhaAtual)
+            {
+                DialogResult resposta = new DialogResult();
+                resposta = MessageBox.Show("Deseja gerar a senha automaticamente?", "Alterar Senha",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resposta == DialogResult.Yes)
+                {
+                    txtNovaSenha.Text = Senha.GerarSenha(8);
+                    txtNovaSenha2.Text = txtNovaSenha.Text;
+                }
+
+                txtNovaSenha.Enabled = true;
+                txtNovaSenha2.Enabled = true;
+                btnOK.Enabled = true;
+                txtNovaSenha.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Senha atual inválida");
+                txtSenhaAtual.Focus();
+            }
+        }
+
+        private void checkBoxMostrarSenha_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxMostrarSenha.Checked)
+            {
+                txtSenhaAtual.PasswordChar = '\u0000';
+                txtNovaSenha.PasswordChar = '\u0000';
+                txtNovaSenha2.PasswordChar = '\u0000';
+            }
+            else
+            {
+                txtSenhaAtual.PasswordChar = '*';
+                txtNovaSenha.PasswordChar = '*';
+                txtNovaSenha2.PasswordChar = '*';
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (txtNovaSenha.Text == txtNovaSenha2.Text)
+                MessageBox.Show("Senha alterada!\nAqui você salva a senha no banco de dados.");
+
+            else
+            {
+                MessageBox.Show("A senha nova digitada não é igual da redigitação.");
+                txtNovaSenha.Focus();
+            }
+        }
+    }
+}
