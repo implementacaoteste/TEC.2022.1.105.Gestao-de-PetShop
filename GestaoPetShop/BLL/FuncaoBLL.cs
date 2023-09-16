b63375beab973ff280aafe08d0b241e346c84d35
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-09-16 15:37:25 -0300

    alteração em função

--- GestaoPetShop/BLL/FuncaoBLL.cs
+++ GestaoPetShop/BLL/FuncaoBLL.cs
@@ -26,7 +26,7 @@ namespace BLL
 
             if (ExisteVinculaDeFuncaoComProfissional(_id))
             {
-                throw new Exception("Esta função não pode ser excluída enquanto tiver vícula com profissional.");
+                throw new Exception("Esta função não pode ser excluída enquanto tiver vínculo com profissional.");
             }
 
             new FuncaoDAL().Excluir(_id);
