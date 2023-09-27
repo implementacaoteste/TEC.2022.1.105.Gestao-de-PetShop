ac24327eca176ff8b0a605bdfe56be092013ef24
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-09-27 20:46:09 -0300

    removendo a linha da autenticacao

--- GestaoPetShop/GestaoPetShop/Program.cs
+++ GestaoPetShop/GestaoPetShop/Program.cs
@@ -17,6 +17,7 @@ namespace GestaoPetShop
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormPrincipal());
+            //Application.Run(new FormPrincipal());
         }
     }
 }
