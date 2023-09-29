281454f108ce68a815663897d80181b46879e79e
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-09-28 21:00:41 -0300

    criando formulario para finalizar

---
+++ GestaoPetShop/GestaoPetShop/FormMainHome.cs
@@ -0,0 +1,162 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestaoPetShop
+{
+    public partial class FormMainHome : Form
+    {
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+        public FormMainHome()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color); 
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    //btnCloseChildForm.Visible = true;
+
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+                }
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPane.Controls.Add(childForm);
+            this.panelDesktopPane.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            labelTitle.Text = childForm.Text;
+           
+        }
+
+
+        private void FormMainHome_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (FormLogin frm = new FormLogin())
+                {
+                    frm.ShowDialog();
+                    if (!frm.Logou)
+                        Application.Exit();
+                    lblUsuarioLogado.Text = Constantes.UsuarioLogado;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonPrincipal_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormPrincipal(), sender);
+        }
+
+        private void buttonUsuarios_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaUsuario(), sender);
+        }
+
+        private void buttonClientes_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaCliente(), sender);
+        }
+
+        private void buttonServiços_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaServico(), sender);
+        }
+
+        private void buttonAgendamento_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaAgendamento(), sender);
+        }
+
+        private void buttonAnimais_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaAnimal(), sender);
+        }
+
+        private void buttonRaca_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaRaca(), sender);
+        }
+
+        private void buttonProfissionais_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaProfissional(), sender);
+        }
+
+        private void buttonFuncao_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormConsultaFuncao(), sender);
+        }
+
+        private void buttonSair_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
