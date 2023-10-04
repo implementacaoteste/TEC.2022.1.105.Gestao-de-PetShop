1c9a45f7155cad993a53fd3b78ba0de16e48fab3
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-10-03 21:01:08 -0300

    inserção do projeto Infra para criptografia e gravar o que é acessado no sistema

---
+++ GestaoPetShop/Infra/Criptografia.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    public class Criptografia
+    {
+        private RSA rsa;
+        public Criptografia()
+        {
+            rsa = RSA.Create();
+        }
+        public string CriptografarSenha(string _senha)
+        {
+            string retorno = _senha;
+            for(int i = 0; i < retorno.Length; i++)
+            {
+                retorno = ReverterTexto(retorno);
+                retorno = GerarHast(retorno);
+            }
+            
+            return GerarHast(retorno);
+        }
+        private string GerarHast(string _texto)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(_texto));
+                return Convert.ToBase64String(hashedBytes);
+            }
+        }
+
+        private string ReverterTexto(string _texto)
+        {
+            char[] charArray = _texto.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
