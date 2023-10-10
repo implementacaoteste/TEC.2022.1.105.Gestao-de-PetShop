3d11b18a9eb7e567ce9aabdf2a7f26f503d3ff43
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-10-10 19:47:58 -0300

    alimentação

--- Banco/SQLQuery1 ALIMENTACAO.sql
+++ Banco/SQLQuery1 ALIMENTACAO.sql
@@ -273,10 +273,10 @@ END
 GO
 
 EXEC SP_CAD_USUARIO 1,'administrador','HjAsWaTFbgNjwwyBrFLS0FiZTF7RQmTvkG13HT3yCZM='
-EXEC SP_CAD_USUARIO 2,'AnaPaula','aten12341'
-EXEC SP_CAD_USUARIO 3, 'Erisvaldo','123'
+EXEC SP_CAD_USUARIO 2,'AnaPaula','QJhhkqDEKtUFhMtxDloEMBIykHVngoWCjhHh0hFWiRQ='
+EXEC SP_CAD_USUARIO 3, 'Erisvaldo','2TbqERhDgvUD7AVPSqQyIZUmHYb9DUyJHiRaOSfUrtA='
 GO
-
+-- Ana Paula ana1234   e Erisvaldo 1234
 /*******************************************************************************************************************************/
 /**************************************INSERIR SITUAÇÃO **************************************************************/
 --use petshop
