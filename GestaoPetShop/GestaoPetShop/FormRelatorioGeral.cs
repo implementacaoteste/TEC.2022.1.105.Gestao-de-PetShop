99e6c8fd58a01dea6db04e1e07d9d4895b18f8f5
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-10-12 15:32:56 -0300

    relatório

--- GestaoPetShop/GestaoPetShop/FormRelatorioGeral.cs
+++ GestaoPetShop/GestaoPetShop/FormRelatorioGeral.cs
@@ -32,10 +32,92 @@ namespace GestaoPetShop
 
         private void buttonRelatorioCliente_Click(object sender, EventArgs e)
         {
-            GerarPdf();
+           
+           
+        GerarPdf();
         }
 
         private void GerarPdf()
+        {
+            try
+            {
+                string datahoje = Convert.ToString(DateTime.Now);
+                datahoje = datahoje.Replace("/", "") ;
+                datahoje = datahoje.Replace(":", "");
+                datahoje = datahoje.Replace(" ", "");
+                string nomeArquivo = @"C:\dados\relatorioCliente"+ datahoje+".pdf";
+                FileStream arquivoPDF = new FileStream(nomeArquivo, FileMode.Create);
+                Document doc = new Document(PageSize.A4);
+                iTextSharp.text.pdf.PdfWriter escritorPDF = iTextSharp.text.pdf.PdfWriter.GetInstance(doc, arquivoPDF);
+
+
+
+                doc.Open();
+                string dados = "";
+
+                iTextSharp.text.Paragraph paragrafo = new iTextSharp.text.Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 14, (int)System.Drawing.FontStyle.Bold));
+
+                paragrafo.Alignment = Element.ALIGN_CENTER;
+                paragrafo.Add("Dimas Martins Mota \n");
+
+                paragrafo.Font = new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 18, (int)System.Drawing.FontStyle.Italic);
+                paragrafo.Alignment = Element.ALIGN_CENTER;
+                paragrafo.Add("Curso: Desenvolvimento de Sistemas \n");
+
+                string email = "E-mail: dimasmartinsmota@hotmail.com";
+                paragrafo.Font = new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 10, (int)System.Drawing.FontStyle.Regular);
+                paragrafo.Alignment = Element.ALIGN_CENTER;
+                paragrafo.Add(email + " \n");
+
+                // inserindo um imagem
+
+                iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(@"C:\dados\trecoimagem.png");
+                logo.ScaleToFit(140f, 120); // tamanho da imagem
+                logo.Alignment = Element.ALIGN_LEFT; // localização da imagem
+                //logo.SetAbsolutePosition(100f, 700f); // outra forma localização da imagem
+
+                // criação de tabelas
+
+                PdfPTable tabela = new PdfPTable(3); // criação de tabela com três colunas
+                tabela.DefaultCell.FixedHeight = 20; // altura da linha da tabela
+
+                PdfPCell celula1 = new PdfPCell(new Phrase("Tabela de preço"));
+                celula1.Colspan = 3; // mescla as três colunas da tabela na 1 linha
+                celula1.Rotation = 90; // rotaciona o texto na primeira linha
+                tabela.AddCell(celula1);
+
+                // Cabeçalho na segunda linha, a escrita vai preenchedo por celula/linha quando uma
+                // linha está completa ela passa para a outra dando continuidade
+                tabela.AddCell("Código");
+                tabela.AddCell("Produto");
+                tabela.AddCell("Preço");
+
+                tabela.AddCell("01");
+                tabela.AddCell("Mause");
+                tabela.AddCell("25,00");
+                tabela.AddCell("02");
+                tabela.AddCell("Teclado");
+                tabela.AddCell("65,00");
+
+
+
+
+
+                doc.Add(logo);
+                doc.Add(paragrafo);
+                doc.Add(tabela);
+                doc.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void btnTeste_Click(object sender, EventArgs e)
         {
             try
             {
@@ -67,11 +149,40 @@ namespace GestaoPetShop
                 // inserindo um imagem
 
                 iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(@"C:\dados\trecoimagem.png");
-                logo.ScaleToFit(140f, 120);
-                logo.Alignment = Element.ALIGN_LEFT;
-                //logo.SetAbsolutePosition(100f, 700f);
+                logo.ScaleToFit(140f, 120); // tamanho da imagem
+                logo.Alignment = Element.ALIGN_LEFT; // localização da imagem
+                //logo.SetAbsolutePosition(100f, 700f); // outra forma localização da imagem
+
+                // criação de tabelas
+
+                PdfPTable tabela = new PdfPTable(3); // criação de tabela com três colunas
+                tabela.DefaultCell.FixedHeight = 20; // altura da linha da tabela
+
+                PdfPCell celula1 = new PdfPCell(new Phrase("Tabela de preço"));
+                celula1.Colspan = 3; // mescla as três colunas da tabela na 1 linha
+                celula1.Rotation = 90; // rotaciona o texto na primeira linha
+                tabela.AddCell(celula1);
+
+                // Cabeçalho na segunda linha, a escrita vai preenchedo por celula/linha quando uma
+                // linha está completa ela passa para a outra dando continuidade
+                tabela.AddCell("Código");
+                tabela.AddCell("Produto");
+                tabela.AddCell("Preço");
+
+                tabela.AddCell("01");
+                tabela.AddCell("Mause");
+                tabela.AddCell("25,00");
+                tabela.AddCell("02");
+                tabela.AddCell("Teclado");
+                tabela.AddCell("65,00");
+
+
+
+
+
                 doc.Add(logo);
                 doc.Add(paragrafo);
+                doc.Add(tabela);
                 doc.Close();
             }
             catch (Exception ex)
