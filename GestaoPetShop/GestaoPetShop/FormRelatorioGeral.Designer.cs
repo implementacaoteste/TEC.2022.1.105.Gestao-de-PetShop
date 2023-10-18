716a5232faf20c0a652ade29678695fa91f8bc8b
Giv@s <giva.com.br@gmail.com>
2023-10-18 08:33:07 -0300

    formatando o layout do formulário relatório

--- GestaoPetShop/GestaoPetShop/FormRelatorioGeral.Designer.cs
+++ GestaoPetShop/GestaoPetShop/FormRelatorioGeral.Designer.cs
@@ -58,12 +58,16 @@
             this.btnTeste.TabIndex = 2;
             this.btnTeste.Text = "teste";
             this.btnTeste.UseVisualStyleBackColor = false;
+            this.btnTeste.Visible = false;
             this.btnTeste.Click += new System.EventHandler(this.btnTeste_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(321, 123);
+            this.pictureBox1.Location = new System.Drawing.Point(329, 179);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(207, 156);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -99,14 +103,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(849, 476);
+            this.ClientSize = new System.Drawing.Size(865, 515);
             this.Controls.Add(this.btnRelacaoDeAgendamentos);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnTeste);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonRelatorioCliente);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormRelatorioGeral";
-            this.Text = "FormRelatorioGeral";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RELATÓRIO GERAL";
             this.Load += new System.EventHandler(this.FormRelatorioGeral_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
